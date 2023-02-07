44e310d752fbd3baef9e53ad4fbe5f12861f7e99
Kendall Kelly <Kendall.kelly@sourcetoad.com>
2023-02-07 09:08:32 -0500

    SIP-385 docs: fixed typo

--- src/types.ts
+++ src/types.ts
@@ -44,8 +44,8 @@ export type DownloadFileOptions = {
   progressDivider?: number;
   begin?: (res: DownloadBeginCallbackResult) => void; // Note: it is required when progress prop provided
   progress?: (res: DownloadProgressCallbackResult) => void;
-  resumable?: () => void; // only supported on iOS yet
-  connectionTimeout?: number; // only supported on Android yet
+  resumable?: () => void; // only supported on iOS 
+  connectionTimeout?: number; // only supported on Android
   readTimeout?: number; // supported on Android and iOS
   backgroundTimeout?: number; // Maximum time (in milliseconds) to download an entire resource (iOS only, useful for timing out background downloads)
 };
